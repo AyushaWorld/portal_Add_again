43157e1fcb24ee167f6dd4b2dd0be25ab75376cb
Usha Thapa <dollop.usha@gmail.com>
2022-02-08 17:41:12 +0530

    react heuroku

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import SignIn from './components/SignIn';
+import {Switch, Route} from 'react-router-dom';
+import ShowBanner from './components/ShowBanner';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+       {/* <SignIn /> */}
+      <Switch>
+        <Route path="/register" component={ShowBanner} />
+
+        <Route exact path="/" component={SignIn} />
+      </Switch>
+    </>
+  )
 }
 
 export default App;
